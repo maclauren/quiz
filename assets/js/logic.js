5764441ac4fe802dcc64452a76b2eea111e41913
lauren-mac <109279133+lauren-mac@users.noreply.github.com>
2023-01-20 19:32:03 +0000

    pull titles/choices/questions add choices/arrays

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+// pull question titles
+const questionTitle = document.getElementById("question-title");
+
+//pull choices
+const choices = document.getElementById("choices");
+
+// pull questions
+const questions = document.getElementById("questions");
+
+// add question titles
+questionTitle.innerText = "Which of these is the odd one out?";
+
+// add choices array
+const choiceArray = ["var", "let", "const", "for"];
+
+// loop for array
+for (let i = 0; i < choiceArray.length; i++) {
+  const choice = document.createElement("li");
+  choice.innerText = choiceArray[i];
+  choices.appendChild(choice);
+}
+
